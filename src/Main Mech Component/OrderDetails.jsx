603447f42a3c7af5a8067f6b5585a5feb7864d89
sohailali31 <158456702+sohailali31@users.noreply.github.com>
2025-04-15 13:01:13 +0530

    Update OrderDetails.jsx

--- src/Main Mech Component/OrderDetails.jsx
+++ src/Main Mech Component/OrderDetails.jsx
@@ -46,7 +46,7 @@ const OrderDetails = ({ onCancel }) => {
   // API base URL
  
  
-  const API_URL = "http://localhost:5522/api"
+  const API_URL = "http://195.35.45.56:5522/api"
  
   // Initial order state for form reset
   const initialOrderState = {
