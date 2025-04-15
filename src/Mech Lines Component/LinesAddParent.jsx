75144c431936c364318c97709edcc6d23cb353f4
bellary infotech solutions <bellaryinfotechsolutions@gmail.com>
2025-04-16 00:57:05 +0530

    Update LinesAddParent.jsx

--- src/Mech Lines Component/LinesAddParent.jsx
+++ src/Mech Lines Component/LinesAddParent.jsx
@@ -45,7 +45,7 @@ const LinesAddParent = ({ onCancel }) => {
   const billToContactRef = useRef(null)
 
   // Update the API_URL to match your code
-  const API_URL = "http://localhost:1111/api"
+  const API_URL = "http://195.35.45.56:5522/api"
 
   // Lookup values state
   const [lookupValues, setLookupValues] = useState({
