5850be8a615e2822d124613c70aa7882c1bd0fe1
bellary infotech solutions <bellaryinfotechsolutions@gmail.com>
2025-04-16 00:57:55 +0530

    Update LinesDatabaseSearch.jsx

--- src/Mech Lines Component/LinesDatabaseSearch.jsx
+++ src/Mech Lines Component/LinesDatabaseSearch.jsx
@@ -16,7 +16,7 @@ const LinesDatabaseSearch = ({ onAddParentClick, onAddChildClick }) => {
   const orderId = 1
 
   // API base URL
-  const API_URL = "http://localhost:1111/api"
+  const API_URL = "http://195.35.45.56:5522/api"
 
   // Fetch order lines from the backend
   useEffect(() => {
